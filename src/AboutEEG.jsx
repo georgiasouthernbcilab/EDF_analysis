226aa24a1d0ea66478cbb340e074adfb071e39e5
Camden Wahl <cj1529.wahl@gmail.com>
2024-07-18 02:13:40 -0400

    Update site and content

--- src/AboutEEG.jsx
+++ src/AboutEEG.jsx
@@ -6,9 +6,9 @@ function AboutEEG() {
 
     useEffect(() => {
         const imageSources = [
-            './example_eeg.png',
-            './bad_electrode_contact.png',
-            './the-10-10-system-new.png'
+            '/example_eeg.png',
+            '/bad_electrode_contact.png',
+            '/the-10-10-system-new.png'
         ];
 
         const loadImages = imageSources.map(src => {
@@ -36,11 +36,11 @@ function AboutEEG() {
                 <p className="bg-[#FFF6F6] rounded p-2 break-words">
                     Electroencephalogram (EEG) is a method of measuring electrical activity in the brain by using small metal electrodes that attach to the scalp. It has been used extensively for the purpose of diagnosing medical conditions and for research purposes. Below is one example of a recorded EEG. This raw graph shows each channels voltage with respect to the point in time and the corresponding electrode.
                 </p>
-                <img src="./example_eeg.png" alt="Example EEG" className="w-full" />
+                <img src="/example_eeg.png" alt="Example EEG" className="w-full" />
                 <p className="bg-[#FFF6F6] rounded p-2 break-words">
                     It&apos;s vital to ensure good contact quality with the scalp of the subject before recording begins. No amount of preprocessing or alterations made after the fact can reconstruct an initially bad signal into a good one. The first step is ensuring good contact quality and that the experimental setup eliminates any kind of motion artifacts. Within EEG, there are many kind of artifacts. These include: heartbeart, lateral eye movement, eye blinks, muscle movement, head movement, electrical interference, clenched jaw, touching the face, among many others. During any experimental setup these are critical to keep in perspective until data collection is complete. The more work done here to prevent these artifacts, the less work will be needed during the preprocessing stage. Below is an example of bad electrode contact with the scalp.
                 </p>
-                <img src="./bad_electrode_contact.png" alt="Bad Electrode Contact" className="w-full" />
+                <img src="/bad_electrode_contact.png" alt="Bad Electrode Contact" className="w-full" />
                 <p className="bg-[#FFF6F6] rounded p-2 break-words">
                     There are a lot of powerful tools out there made for analyzing EEG recordings such as MNE-Python and EEGLAB within MATLAB. There are also several different considerations that have to be made when considering the EEG headset being employed. In our case, we will be utilizing the <a href="https://emotiv.gitbook.io/epoc-flex-user-manual/epoc-flex/readme" target="_blank" className="text-[#598392]">Emotiv Flex 32-electrode system with saline sensors.</a> Please review the technical specifications and ensure that when you are working within your development environment that you match the sampling rate to the correct number. With this model, you can either record data at 128 samples per second or 256 if using the cable.
                 </p>
@@ -48,7 +48,7 @@ function AboutEEG() {
                 <p className="bg-[#FFF6F6] rounded p-2 break-words">
                     To properly start the preprocessing stage, you&apos;ll need to understand electrode position conventions. Please review the <a href="https://en.wikipedia.org/wiki/10%E2%80%9320_system_(EEG)" target='_blank' className="text-[#598392]">10-20 system.</a> We will be working under the assumption of this principle and our analysis in MNE-Python will also adhere to the 10-20 system.
                 </p>
-                <img src="./the-10-10-system-new.png" alt="The 10-20 System" className="w-full mt-2" />
+                <img src="/the-10-10-system-new.png" alt="The 10-20 System" className="w-full mt-2" />
             </div>
         </div>
     );
--- src/Home.jsx
+++ src/Home.jsx
@@ -8,7 +8,7 @@ function Home() {
             <p className = "mt-10">To get started, please visit the MNE introductory <a href="https://mne.tools/stable/auto_tutorials/intro/index.html" target="_blank" className="text-[#598392] ">tutorial</a> and familiarize yourself with the content.</p>
             <p className = "">If you need to install MNE-Python, please refer to their <a href="https://mne.tools/stable/install/index.html" target="_blank" className="text-[#598392] ">installation page.</a></p>
             <h2 className = "text-2xl mt-10">At the time of these tutorials, we were using Python 3.12.8 and version 1.7.1 of MNE-Python.</h2>
-            <img src="./georgia-southern-eagles3289.jpg" alt="Georgia Southern Logo"  className="w-1/2"/>
+            <img src="/georgia-southern-eagles3289.jpg" alt="Georgia Southern Logo"  className="w-1/2"/>
         </div>
         </>
     )
