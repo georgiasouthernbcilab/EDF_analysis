287b26f3173a2bb1ca4518513e88336b93ee6759
Camden Wahl <cj1529.wahl@gmail.com>
2024-08-06 12:25:52 -0400

    Update src

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,9 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 import {
-  HashRouter,
+  createHashRouter,
   RouterProvider,
-} from "react-router-dom";
+} from 'react-router-dom';
 import Navigation from './Navigation.jsx';
 import AboutEEG from './AboutEEG.jsx';
 import Preprocessing from './Preprocessing.jsx';
@@ -12,51 +12,57 @@ import Home from './Home.jsx';
 import Preprocessing_2 from './Preprocessing_2.jsx';
 import Preprocessing_3 from './Preprocessing_3.jsx';
 
-const router = HashRouter([
+// Create a hash router
+const router = createHashRouter([
   {
     path: "/",
-    element: 
-    <>
-    <Navigation/>
-    <Home/>
-    </>,
+    element: (
+      <>
+        <Navigation />
+        <Home />
+      </>
+    ),
   },
   {
     path: "/About_EEG",
-    element: 
-    <>
-    <Navigation />
-    <AboutEEG />
-    </>
+    element: (
+      <>
+        <Navigation />
+        <AboutEEG />
+      </>
+    ),
   },
   {
     path: "/Preprocessing",
-    element:
-    <>
-    <Navigation />
-    <Preprocessing />
-    </>
+    element: (
+      <>
+        <Navigation />
+        <Preprocessing />
+      </>
+    ),
   },
   {
     path: "/Preprocessing_2",
-    element:
-    <>
-    <Navigation />
-    <Preprocessing_2 />
-    </>
+    element: (
+      <>
+        <Navigation />
+        <Preprocessing_2 />
+      </>
+    ),
   },
   {
     path: "/Preprocessing_3",
-    element:
-    <>
-    <Navigation />
-    <Preprocessing_3 />
-    </>
-  }
+    element: (
+      <>
+        <Navigation />
+        <Preprocessing_3 />
+      </>
+    ),
+  },
 ]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <RouterProvider router={router} />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
